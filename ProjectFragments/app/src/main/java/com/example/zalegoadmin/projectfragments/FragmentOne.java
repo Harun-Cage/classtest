076b3ce8a5f1b23b89a141fa5d8be8d10ac51a8f
HarunCage <hkgatutha@gmail.com>
2017-03-29 13:36:51 +0300

    my first commit

---
+++ ProjectFragments/app/src/main/java/com/example/zalegoadmin/projectfragments/FragmentOne.java
@@ -0,0 +1,22 @@
+package com.example.zalegoadmin.projectfragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ZalegoAdmin on 3/8/2017.
+ */
+public class FragmentOne extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_one,container,false);
+        return  view;
+
+    }
+}
+
---
+++ ProjectFragments/app/src/main/java/com/example/zalegoadmin/projectfragments/FragmentThree.java
@@ -0,0 +1,21 @@
+package com.example.zalegoadmin.projectfragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ZalegoAdmin on 3/8/2017.
+ */
+public class FragmentThree extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_three,container,false);
+        return view;
+    }
+}
+
